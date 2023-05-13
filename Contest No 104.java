b69063534c5d99add9cf761202abc5e5f399d942
skya3 <107419125+PoojaGadagi08@users.noreply.github.com>
2023-05-13 21:37:38 +0530

    Update Contest No 104.java

--- Contest No 104.java
+++ Contest No 104.java
@@ -1,4 +1,4 @@
-// Number of Senior Citizens
+//1) Number of Senior Citizens
 
 
 // User Accepted:12258
@@ -26,3 +26,48 @@ class Solution {
     }
 }
 
+
+
+// 2)Sum in a Matrix
+
+
+// User Accepted:9880
+// User Tried:11243
+// Total Accepted:10098
+// Total Submissions:18196
+// Difficulty:Medium
+// You are given a 0-indexed 2D integer array nums. Initially, your score is 0. Perform the following operations until the matrix becomes empty:
+
+// From each row in the matrix, select the largest number and remove it. In the case of a tie, it does not matter which number is chosen.
+// Identify the highest number amongst all those removed in step 1. Add that number to your score.
+// Return the final score.
+
+class Solution {
+    public int matrixSum(int[][] a) {
+        int len=a.length;
+        
+        //PrioriotyQueue<Integer> m=new PrioriotyQueue<>(Collections.reverseOrder());
+        List<PriorityQueue<Integer>> l=new ArrayList<>();
+        
+        for(int i=0;i<len;i++){
+             PriorityQueue<Integer> max=new PriorityQueue<>(Collections.reverseOrder());
+            
+            for(int j=0;j<a[0].length;j++){
+                max.add(a[i][j]);
+            }
+            l.add(max);
+        }
+        int max=0;
+        int s=0;
+        for(int i=0;i<a[0].length;i++){
+            int mxx=0;
+            for(int j=0;j<l.size();j++){
+                int mx=l.get(j).poll();
+                if(mx>mxx)
+                    mxx=mx;
+            }
+            s+=mxx;
+        }
+        return s;
+    }
+}
